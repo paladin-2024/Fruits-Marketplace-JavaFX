d4eb30e2fdf734c801d0f864223b4a54bfff1e60
paladin-2024 <cnzabb@gmail.com>
2025-06-28 20:47:38 +0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.fruits_market_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.fruits_market_app to javafx.fxml;
+    exports org.example.fruits_market_app;
+}
