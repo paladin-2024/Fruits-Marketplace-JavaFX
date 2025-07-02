25277ccaa0aa11d15eb3a295be73ad55ecafbd20
paladin-2024 <cnzabb@gmail.com>
2025-07-03 02:37:09 +0300

    feat: enhance market layout with search functionality and styling

--- src/main/java/org/example/fruits_market_app/main/Main.java
+++ src/main/java/org/example/fruits_market_app/main/Main.java
@@ -2,18 +2,19 @@ package org.example.fruits_market_app.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(".../resources/views/market.fxml")));
+        stage.setTitle("FruityMart");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
